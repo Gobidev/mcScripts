39066fca4645136ead1915c839f3b1e3383e33c8
Adrian Groh <adrian.groh@t-online.de>
2021-12-02 15:10:46 +0100

    Add option to not use aikar flags

--- start.sh
+++ start.sh
@@ -5,9 +5,13 @@
 # name of server screen, has to be the same in stop.sh and backup.sh
 screen_name=MinecraftServer
 # executable jar file that starts the server
-jar_name=paper.jar
+jar_name=fabric-server-launch.jar
+#jar_name=paper.jar
 # max. RAM in MB dedicated to the server, recommended is 1000-1500M less than your physical memory
 ram_m=4000
+# specify if you want to use aikar flags
+aikar_flags=n
+
 
 # ###############################################
 
@@ -33,26 +37,32 @@ then
 else
 	echo "No running server found, starting.."
 
-	if [ $ram_m -lt 12000 ]
+	if [ $aikar_flags == "n" ]
 	then
-	  echo "Xmx is less than 12G, running default Aikar flags"
-	  screen -AmdS "$screen_name" java -Xms${ram_m}M -Xmx${ram_m}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
-	  -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
-	  -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
-	  -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
-	  -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
-	  -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs \
-	  -Daikars.new.flags=true -jar $jar_name nogui
-  else
-    echo "Xmx is greater than 12G, running modified Aikar flags"
-    screen -AmdS "$screen_name" java -Xms${ram_m}M -Xmx${ram_m}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
-	  -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
-	  -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 \
-	  -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 \
-	  -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
-	  -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs \
-	  -Daikars.new.flags=true -jar $jar_name nogui
-  fi
+		echo "Running without Aikar flags"
+		screen -AmdS "$screen_name" java -Xms${ram_m}M -Xmx${ram_m}M -jar $jar_name nogui
+	else
+		if [ $ram_m -lt 12000 ]
+		then
+	  		echo "Xmx is less than 12G, running default Aikar flags"
+	  		screen -AmdS "$screen_name" java -Xms${ram_m}M -Xmx${ram_m}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+			-XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
+			-XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
+			-XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
+			-XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
+			-XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs \
+			-Daikars.new.flags=true -jar $jar_name nogui
+  		else
+			echo "Xmx is greater than 12G, running modified Aikar flags"
+			screen -AmdS "$screen_name" java -Xms${ram_m}M -Xmx${ram_m}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+			-XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
+			-XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 \
+			-XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 \
+			-XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
+			-XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs \
+			-Daikars.new.flags=true -jar $jar_name nogui
+  		fi
+	fi
 
 	# check if screen is running after 5s
 	sleep 5s
